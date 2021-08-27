695682245ee8801a98fe7f81ca3c57b80fe30413
Alexander Korshunov <71963376+akorshunov60@users.noreply.github.com>
2021-08-27 20:38:14 +0300

    Initial commit

---
+++ src/main/java/com/example/springcloudk2a/SpringCloudk2aApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudk2a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudk2aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudk2aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcloudk2a/SpringCloudk2aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudk2a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudk2aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
