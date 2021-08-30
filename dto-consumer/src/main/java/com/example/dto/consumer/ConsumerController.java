726f3b39f6e900566cf334b1732abf9ff817b293
Alexander Korshunov <71963376+akorshunov60@users.noreply.github.com>
2021-08-30 19:29:09 +0300

    см.файл lesson-5.md

--- dto-consumer/src/main/java/com/example/dto/consumer/ConsumerController.java
+++ dto-consumer/src/main/java/com/example/dto/consumer/ConsumerController.java
@@ -1,22 +1,36 @@
 package com.example.dto.consumer;
 
-import lombok.RequiredArgsConstructor;
+import com.example.dto.common.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Optional;
 
-@RequiredArgsConstructor
-@RestController
+@Controller
 public class ConsumerController {
 
-    private final RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping("/products")
-    public String showProducts() {
-        Optional<List> products = Optional.ofNullable(restTemplate.getForObject("http://localhost:8189/", List.class));
-        return "products" + products;
+    public String showProduct(Model model) {
+        ResponseEntity<List> restExchange = restTemplate.exchange("http://localhost:8189/", HttpMethod.GET, null, List.class);
+        List<ProductDTO> products = (List<ProductDTO>)restExchange.getBody();
+        model.addAttribute("products", products);
+        return "products";
     }
+
+//    @GetMapping("/products")
+//    public List<ProductDTO> showProducts() {
+//        return restTemplate.getForObject("http://localhost:8189/", List.class);
+//    }
 }
